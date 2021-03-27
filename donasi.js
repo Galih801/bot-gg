de1cab323aec003b19ce20396f5d8555df68d218
Galih801 <81460883+Galih801@users.noreply.github.com>
2021-03-27 19:42:23 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,13 @@
+const donasi = () => { 
+	return `       
+┌──「 *DONASI BOT* 」
+│
+├◪ *DONASI BISA MELALUI :*
+├◪ *GOPAY : 089530863358*
+├◪ *DANA : 089530863358*
+├◪ *OVO : 089530863358*
+│
+└──「 *GALIH BOT* 」
+}
+
+exports.donasi = donasi
